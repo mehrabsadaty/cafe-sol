764c4b92e3fa30e14b3a00781d6cc083a4afc129
mehrabsadaty <seiedmehrabsadati@gmail.com>
2025-03-09 00:07:01 +0330

    final commit 1

---
+++ JavaScript/js.js
@@ -0,0 +1,31 @@
+function handelclick(idname){
+    let button = document.getElementById(idname);
+
+    if (!button.hasOwnProperty('flag')) {
+        button.flag = true;
+    }
+
+    if (button.flag) {
+        button.style.backgroundColor = "rgb(192, 134, 71)";
+        button.style.boxShadow = "3px 3px 4px -1px inset rgb(0 , 0, 0)";
+        button.style.color = "black";
+        button.flag = false;   
+    }
+    else{
+        button.style.backgroundColor = "#542f05";
+        button.style.boxShadow = "3px 3px 4px -1px rgb(0, 0, 0)";
+        button.style.color = "wheat";
+
+        button.flag = true;
+    }
+}
+let ids = ["shak", "summer"];
+
+ids.forEach(id => {
+    let element = document.getElementById(id);
+    if (element) {
+        element.style.backgroundColor = "rgb(76, 70, 70)";
+        element.style.color = "rgb(36, 32, 32)";
+        element.style.boxShadow = "3px 3px 4px -1px inset rgb(0 , 0, 0)";
+    }
+});
